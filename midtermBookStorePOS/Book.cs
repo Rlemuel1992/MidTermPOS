57bc9bc84a4ded11b2fb5502d93442e17ccb783f
Rudy Gall <rudyegall@gmail.com>
2019-01-31 17:01:04 -0500

    Add Menu

---
+++ midtermBookStorePOS/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midtermBookStorePOS
+{
+    class Book
+    {
+        public Book()
+        {
+
+        }
+        public Book(string title, string author, double price)
+        {
+            Price = price;
+            Title = title;
+            Author = author;
+        }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public string Author { get; set; }
+        }
+}
---
+++ midtermBookStorePOS/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midtermBookStorePOS
+{
+    class Menu
+    {
+        public static void DisplayMenu()
+        {
+            int userNumSelectMenu;
+            Console.WriteLine("Please choose a number of the following options:\n");
+            Console.WriteLine("1. In Search Of Lost Time By Marcel Proust\n2. Don Quixote By Miguel De Cer3vantes\n3. Ulysses By James Joyce\n4. The Great Gatsby By F.Scott Fitzgerald\n5. Moby Dick By Herman Melville\n6. Hamlet By William Shakespeare\n7. War and Peace By Leo Tolstoy\n8. The Odyssey By Homer\n9. Lolita By Vladimir Nabokov\n10. On the Road By Jack Kerouac\n11. Breakfast of Champions By Kurt Vonnegut\n12. The Alchemist By Paulo Coelho");
+            Console.WriteLine("\nPlease enter a menu number");
+            userNumSelectMenu = Validation.SelectNum(); //Calling class of validation to select number of book
+
+            if (userNumSelectMenu == Book)
+            {
+
+            }
+        }
+    }
+}
