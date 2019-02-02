919972b25aa963793f4d8687c6672b85511358ae
Rudy Gall <rudyegall@gmail.com>
2019-02-02 16:27:34 -0500

    Fixed property error

--- midtermBookStorePOS/Menu.cs
+++ midtermBookStorePOS/Menu.cs
@@ -15,29 +15,29 @@ namespace midtermBookStorePOS
         public static void DisplayMenu()
         {
             List<Book> Books = new List<Book>();
-            Book InSearchofLostTime = new Book("In Search of Lost Time", "Marcel Proust", 22.75);
+            Book InSearchofLostTime = new Book("In Search of Lost Time", "Marcel Proust", 22.75,0);
             Books.Add(InSearchofLostTime);
-            Book DonQuixote = new Book("Don Quixote", "Miguel de Cervantes", 29.99);
+            Book DonQuixote = new Book("Don Quixote", "Miguel de Cervantes", 29.99,0);
             Books.Add(DonQuixote);
-            Book Ulysses = new Book("Ulysses", "James Joyce", 15.95);
+            Book Ulysses = new Book("Ulysses", "James Joyce", 15.95,0);
             Books.Add(Ulysses);
-            Book TheGreatGatsby = new Book("The Great Gatsby", "F.Scott Fitzgerald", 10.82);
+            Book TheGreatGatsby = new Book("The Great Gatsby", "F.Scott Fitzgerald", 10.82,0);
             Books.Add(TheGreatGatsby);
-            Book MobyDick = new Book("Moby Dick", "Herman Melville", 9.56);
+            Book MobyDick = new Book("Moby Dick", "Herman Melville", 9.56,0);
             Books.Add(MobyDick);
-            Book Hamlet = new Book("Hamlet", "William Shakespeare", 12.95);
+            Book Hamlet = new Book("Hamlet", "William Shakespeare", 12.95,0);
             Books.Add(Hamlet);
-            Book WarandPeace = new Book("War and Peace", "Leo Tolstoy", 11.16);
+            Book WarandPeace = new Book("War and Peace", "Leo Tolstoy", 11.16,0);
             Books.Add(WarandPeace);
-            Book TheOdyssey = new Book("The Odyssey", "Homer", 14.39);
+            Book TheOdyssey = new Book("The Odyssey", "Homer", 14.39,0);
             Books.Add(TheOdyssey);
-            Book Lolita = new Book("Lolita", "Vladimir Nabokov", 17.00);
+            Book Lolita = new Book("Lolita", "Vladimir Nabokov", 17.00,0);
             Books.Add(Lolita);
-            Book OntheRoad = new Book("On the Road", "Jack Kerouac", 14.45);
+            Book OntheRoad = new Book("On the Road", "Jack Kerouac", 14.45,0);
             Books.Add(OntheRoad);
-            Book Breakfastofchampions = new Book("Breakfast of champions", "Kurt Vonnegut", 15.35);
+            Book Breakfastofchampions = new Book("Breakfast of champions", "Kurt Vonnegut", 15.35,0);
             Books.Add(Breakfastofchampions);
-            Book TheAlchemist = new Book("The Alchemist", "Paulo Coelho", 13.89);
+            Book TheAlchemist = new Book("The Alchemist", "Paulo Coelho", 13.89,0);
             Books.Add(TheAlchemist);
 
             int userNumSelectMenu;
