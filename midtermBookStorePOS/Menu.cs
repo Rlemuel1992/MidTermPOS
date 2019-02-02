efedb1aba01b25ea7be984d3907749665ded3e8f
Rudy Gall <rudyegall@gmail.com>
2019-02-02 15:57:14 -0500

    Added back to menu or checkout

--- midtermBookStorePOS/Menu.cs
+++ midtermBookStorePOS/Menu.cs
@@ -10,7 +10,7 @@ namespace midtermBookStorePOS
     {                           //Rudy-Add descriptions of books
 
         public double Price { get; internal set; }
-        
+
 
         public static void DisplayMenu()
         {
@@ -44,103 +44,139 @@ namespace midtermBookStorePOS
             bool isValidInput = false;
             int quantity = 0;
             Checkout c = new Checkout(quantity);
+            bool toMenu = true;
 
-            
+            while (toMenu == true)
+            {
+                Console.WriteLine("Please choose a number of the following options:\n");
+                Console.WriteLine("1. In Search Of Lost Time By Marcel Proust\n2. Don Quixote By Miguel De Cervantes\n3. Ulysses By James Joyce\n4. The Great Gatsby By F.Scott Fitzgerald\n5. Moby Dick By Herman Melville\n6. Hamlet By William Shakespeare\n7. War and Peace By Leo Tolstoy\n8. The Odyssey By Homer\n9. Lolita By Vladimir Nabokov\n10. On the Road By Jack Kerouac\n11. Breakfast of Champions By Kurt Vonnegut\n12. The Alchemist By Paulo Coelho");
+                Console.WriteLine("\nPlease enter a menu number");
+                userNumSelectMenu = Validation.SelectNum(); //Calling class of validation to select number of book
 
-            Console.WriteLine("Please choose a number of the following options:\n");
-            Console.WriteLine("1. In Search Of Lost Time By Marcel Proust\n2. Don Quixote By Miguel De Cervantes\n3. Ulysses By James Joyce\n4. The Great Gatsby By F.Scott Fitzgerald\n5. Moby Dick By Herman Melville\n6. Hamlet By William Shakespeare\n7. War and Peace By Leo Tolstoy\n8. The Odyssey By Homer\n9. Lolita By Vladimir Nabokov\n10. On the Road By Jack Kerouac\n11. Breakfast of Champions By Kurt Vonnegut\n12. The Alchemist By Paulo Coelho");
-            Console.WriteLine("\nPlease enter a menu number");
-            userNumSelectMenu = Validation.SelectNum(); //Calling class of validation to select number of book
-
-            if (userNumSelectMenu == 1)
-            {
-                Console.WriteLine(InSearchofLostTime.Title + ", " + InSearchofLostTime.Author + ", $" + InSearchofLostTime.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(InSearchofLostTime);
+                if (userNumSelectMenu == 1)
+                {
+                    Console.WriteLine(InSearchofLostTime.Title + ", " + InSearchofLostTime.Author + ", $" + InSearchofLostTime.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(InSearchofLostTime);
+                    toMenu = true;
+                }
+                else if (userNumSelectMenu == 2)
+                {
+                    Console.WriteLine(DonQuixote.Title + ", " + DonQuixote.Author + ", $" + DonQuixote.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(DonQuixote);
+                    toMenu = true;
+                }
+                else if (userNumSelectMenu == 3)
+                {
+                    Console.WriteLine(Ulysses.Title + ", " + Ulysses.Author + ", $" + Ulysses.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(Ulysses);
+                    toMenu = true;
+                }
+                else if (userNumSelectMenu == 4)
+                {
+                    Console.WriteLine(TheGreatGatsby.Title + ", " + TheGreatGatsby.Author + ", $" + TheGreatGatsby.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(TheGreatGatsby);
+                    toMenu = true;
+                }
+                else if (userNumSelectMenu == 5)
+                {
+                    Console.WriteLine(MobyDick.Title + ", " + MobyDick.Author + ", $" + MobyDick.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(MobyDick);
+                    toMenu = true;
+                }
+                else if (userNumSelectMenu == 6)
+                {
+                    Console.WriteLine(Hamlet.Title + ", " + Hamlet.Author + ", $" + Hamlet.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(Hamlet);
+                    toMenu = true;
+                }
+                else if (userNumSelectMenu == 7)
+                {
+                    Console.WriteLine(WarandPeace.Title + ", " + WarandPeace.Author + ", $" + WarandPeace.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(WarandPeace);
+                    toMenu = true;
+                }
+                else if (userNumSelectMenu == 8)
+                {
+                    Console.WriteLine(TheOdyssey.Title + ", " + TheOdyssey.Author + ", $" + TheOdyssey.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(TheOdyssey);
+                    toMenu = true;
+                }
+                else if (userNumSelectMenu == 9)
+                {
+                    Console.WriteLine(Lolita.Title + ", " + Lolita.Author + ", $" + Lolita.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(Lolita);
+                    toMenu = true;
+                }
+                else if (userNumSelectMenu == 10)
+                {
+                    Console.WriteLine(OntheRoad.Title + ", " + OntheRoad.Author + ", $" + OntheRoad.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(OntheRoad);
+                    toMenu = true;
+                }
+                else if (userNumSelectMenu == 11)
+                {
+                    Console.WriteLine(Breakfastofchampions.Title + ", " + Breakfastofchampions.Author + ", $" + Breakfastofchampions.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(Breakfastofchampions);
+                    toMenu = true;
+                }
+                else if (userNumSelectMenu == 12)
+                {
+                    Console.WriteLine(TheAlchemist.Title + ", " + TheAlchemist.Author + ", $" + TheAlchemist.Price);
+                    Console.WriteLine("Please input the amount of books you'd like to order.");
+                    quantity = int.Parse(Console.ReadLine());
+                    Program.Cart.Add(TheAlchemist);
+                    toMenu = true;
+                }
+                else
+                {
+                    Console.WriteLine("That's not between 1-12. Try Again.");
+                }
+                while (isValidInput);
+                toMenu = backToMenu();
             }
-            else if (userNumSelectMenu == 2)
+        }
+        public static bool backToMenu()
+        {
+            Console.WriteLine("\nWould you like to return to the Menu or proceed to Checkout?");
+            string input = Console.ReadLine().ToLower();
+            input = input.ToLower();
+            bool goOn;
+            if (input == "menu")
             {
-                Console.WriteLine(DonQuixote.Title + ", " + DonQuixote.Author + ", $" + DonQuixote.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(DonQuixote);
+                goOn = true;
             }
-            else if (userNumSelectMenu == 3)
+            else if (input == "checkout")
             {
-                Console.WriteLine(Ulysses.Title + ", " + Ulysses.Author + ", $" + Ulysses.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(Ulysses);
-            }
-            else if (userNumSelectMenu == 4)
-            {
-                Console.WriteLine(TheGreatGatsby.Title + ", " + TheGreatGatsby.Author + ", $" + TheGreatGatsby.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(TheGreatGatsby);
-            }
-            else if (userNumSelectMenu == 5)
-            {
-                Console.WriteLine(MobyDick.Title + ", " + MobyDick.Author + ", $" + MobyDick.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(MobyDick);
-            }
-            else if (userNumSelectMenu == 6)
-            {
-                Console.WriteLine(Hamlet.Title + ", " + Hamlet.Author + ", $" + Hamlet.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(Hamlet);
-            }
-            else if (userNumSelectMenu == 7)
-            {
-                Console.WriteLine(WarandPeace.Title + ", " + WarandPeace.Author + ", $" + WarandPeace.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(WarandPeace);
-            }
-            else if (userNumSelectMenu == 8)
-            {
-                Console.WriteLine(TheOdyssey.Title + ", " + TheOdyssey.Author + ", $" + TheOdyssey.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(TheOdyssey);
-            }
-            else if (userNumSelectMenu == 9)
-            {
-                Console.WriteLine(Lolita.Title + ", " + Lolita.Author + ", $" + Lolita.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(Lolita);
-            }
-            else if (userNumSelectMenu == 10)
-            {
-                Console.WriteLine(OntheRoad.Title + ", " + OntheRoad.Author + ", $" + OntheRoad.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(OntheRoad);
-            }
-            else if (userNumSelectMenu == 11)
-            {
-                Console.WriteLine(Breakfastofchampions.Title + ", " + Breakfastofchampions.Author + ", $" + Breakfastofchampions.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(Breakfastofchampions);
-            }
-            else if (userNumSelectMenu == 12)
-            {
-                Console.WriteLine(TheAlchemist.Title + ", " + TheAlchemist.Author + ", $" + TheAlchemist.Price);
-                Console.WriteLine("Please input the amount of books you'd like to order.");
-                quantity = int.Parse(Console.ReadLine());
-                Program.Cart.Add(TheAlchemist);
+                goOn = false;
             }
             else
             {
-                Console.WriteLine("That's not between 1-12. Try Again.");
+                Console.WriteLine("I don't understand that, let's try again");
+                goOn = backToMenu();
             }
-            while (isValidInput) ;
+            return goOn;
         }
     }
 }
